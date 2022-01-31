70750e8005bdd29fdb23cff38a0307168a2bf0e2
GrowlyX <growlyxdev@gmail.com>
2022-01-31 17:55:38 -0500

    Added input polling logic

--- src/main/kotlin/gg/growly/randomizer/Randomizer.kt
+++ src/main/kotlin/gg/growly/randomizer/Randomizer.kt
@@ -5,7 +5,7 @@ import java.io.InputStreamReader
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.util.stream.Collectors
-
+import kotlin.concurrent.thread
 
 /**
  * @author GrowlyX
@@ -19,10 +19,61 @@ object Randomizer
     @JvmStatic
     fun main(args: Array<String>)
     {
+        val act = "What act would you like to cover?".response()
+        val scene = "What scene would you like to cover?".response()
+
         val unparsed = Files
-            .lines(Paths.get("${"What act would you like to cover?".response()}-${"What scene would you like to cover?".response()}.txt"))
+            .lines(Paths.get("$act-$scene.txt"))
             .collect(Collectors.toList())
 
+        parseText(unparsed)
+
+        thread {
+            while (true)
+                pollInput()
+        }
+    }
+
+    private fun pollInput()
+    {
+        val mapping = quoteMappings
+            .entries.random()
+
+        val speaker = mapping.key
+        val quote = mapping.value.random()
+
+        val randomSpeakers = quoteMappings.keys
+            .filter { it != speaker }.take(3)
+            .toMutableList().also {
+                it.add(speaker)
+                it.shuffle()
+            }
+
+        val description = mutableListOf<String>()
+        description.add("")
+        description.add("Who said this?")
+        description.add(quote)
+        description.add("")
+
+        randomSpeakers.forEachIndexed { index, random ->
+            description.add("#${index + 1}. $random")
+        }
+
+        description.add("")
+
+        val response = description.response()
+
+        if (response.equals(speaker, true))
+        {
+            println("✔ You got it correct!")
+        } else
+        {
+            println("❌ Wrong! Correct answer: $speaker")
+        }
+    }
+
+    private fun parseText(unparsed: List<String>)
+    {
         var startIndex = 0
         var current = ""
 
@@ -43,7 +94,7 @@ object Randomizer
             if (startIndex == index)
             {
                 quoteMappings[value] ?:
-                    quoteMappings.put(value, mutableListOf())
+                quoteMappings.put(value, mutableListOf())
 
                 current = value
             } else
@@ -51,23 +102,21 @@ object Randomizer
                 quoteMappings[current]?.add(value)
             }
         }
-
-        for (entry in quoteMappings.entries)
-        {
-            println("${entry.key}:")
-
-            entry.value
-                .forEach { println(" $it") }
-        }
     }
+
+    private val reader = BufferedReader(
+        InputStreamReader(System.`in`)
+    )
 
     private fun String.response(): String
     {
-        val reader = BufferedReader(
-            InputStreamReader(System.`in`)
-        )
-
         println("$this ")
+        return reader.readLine()
+    }
+
+    private fun List<String>.response(): String
+    {
+        forEach { println(it) }
         return reader.readLine()
     }
 }
